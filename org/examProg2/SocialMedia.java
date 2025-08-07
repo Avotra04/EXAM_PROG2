7d35ac2b3f78aa88fcbf6424e3db3767c0ae1424
Avotra <hei.avotra@gmail.com>
2025-08-07 09:39:54 +0300

    rectification de Users avec void

--- org/examProg2/SocialMedia.java
+++ org/examProg2/SocialMedia.java
@@ -6,7 +6,7 @@ public class SocialMedia {
     String moderator;
     String admin;
 
-    public Users(String userStandar, String moderator, String admin) {
+    public void Users(String userStandar, String moderator, String admin) {
         this.userStandar = userStandar;
         this.moderator = moderator;
         this.admin = admin;
