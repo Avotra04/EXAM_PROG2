62a37a6b0d04d8056311b87d1411de3c83721fd3
Avotra <hei.avotra@gmail.com>
2025-08-07 09:57:57 +0300

    insertion des type et override

--- org/examProg2/Function.java
+++ org/examProg2/Function.java
@@ -1,4 +1,38 @@
 package org.examProg2;
 
-public class Function {
+public class Function extends SocialMedia {
+    String comment;
+    String post;
+    String delete;
+    String create;
+
+    public Function(String comment, String post, String delete) {
+    this.comment = comment;
+    this.post = post;
+    this.delete = delete;
+    }
+
+    @Override
+    public void Users(String userStandar, String moderator, String admin) {
+        super.Users(userStandar, moderator, admin);
+    }
+
+    @Override
+    public String getUserStandar() {
+        return super.getUserStandar();
+    }
+
+    @Override
+    public void setUserStandar(String userStandar) {
+        super.setUserStandar(userStandar);
+    }
+
+    @Override
+    public String getAdmin() {
+        return super.getAdmin();
+    }
+
 }
+
+
+
