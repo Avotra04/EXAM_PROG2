f8dec0dc488d40b3fcbcc7a424dd0a7293cdcb8a
Avotra <hei.avotra@gmail.com>
2025-08-07 09:07:28 +0300

    definition des attribut avec this

--- org/examProg2/SocialMedia.java
+++ org/examProg2/SocialMedia.java
@@ -1,4 +1,16 @@
 package org.examProg2;
 
 public class SocialMedia {
-}
+
+    String userStandar;
+    String moderator;
+    String admin;
+
+    public Users(String userStandar, String moderator, String admin) {
+        this.userStandar = userStandar;
+        this.moderator = moderator;
+        this.admin = admin;
+    }
+
+
+
